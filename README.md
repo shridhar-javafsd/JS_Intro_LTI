41c265eeaf74c3b5381c4f570dfb0986426a0752
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-14 10:54:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_Intro
+ JS Intro for LTI trainees Dec 2021
---
+++ my-script.js
@@ -0,0 +1,43 @@
+
+console.log("Hello world!");
+
+// run as 
+// node my-script.js 
+
+// create a function here and invoke it. 
+
+// function fun() {
+//     console.log("fun function output");
+// } 
+
+let fun = () => {
+    console.log("fun function output");
+}
+
+fun();
+
+let addNums = (num1, num2) => {
+    console.log(num1 + num2);
+}
+
+addNums(10, 20); // 30 
+addNums(10); // ?
+addNums(10, 20, 30); // e 
+
+// var   
+let accountBalance = 100;  
+const myIfsc = `aaabbcc1234`;
+
+
+// Web intro 
+
+// HTML - language to render (display) text and media on browser 
+// CSS - style for HTML 
+// JavaScript - logic for HTML 
+
+
+
+
+
+
+
